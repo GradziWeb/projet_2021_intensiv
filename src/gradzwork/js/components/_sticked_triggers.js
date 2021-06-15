d982dec69570c01081e4650517d027069521a8ac
Gradzi <Gradzy7@gmail.com>
2021-06-15 22:33:00 +0200

    projet version juin

---
+++ src/gradzwork/js/components/_sticked_triggers.js
@@ -0,0 +1,13 @@
+// TEST
+/*
+window.addEventListener('scroll', (e) => {
+    document.querySelectorAll('.sticky').forEach((sticky) => {
+        if () {
+            sticky.classList.add('');
+        }
+        else {
+            sticky.classList.remove('');
+        }
+    });
+});
+*/
---
+++ src/gradzwork/js/components/_toogle_menu.js
@@ -0,0 +1,21 @@
+
+//TEST
+/*
+function toggleVisible() {
+  const btn = document.querySelectorAll();
+  for (let btns of btn) {
+    btn.addEventListener('click', function() {
+      this.querySelector().classList.toggle('visible');
+      removeVisible();
+    });
+  }
+}
+
+function removeVisible(){
+  window.addEventListener('click', function(e) {
+    if () {
+      document.querySelector().classList.remove('visible');
+    }
+  });
+}
+*/
